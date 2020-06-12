524fdf491a45bb036eeb6f8f52140e794798c3f5
peter-lancaster <lancasterp2@hotmail.co.uk>
2020-06-12 12:11:51 +0100

    Added in Switch and Route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from "./Header"
 import Footer from "./Footer"
+import {Switch, Route} from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+import Services from "./components/Services"
+import Contact from "./components/Contact"
 
 function App() {
   return (
     <>
     <Header />
-    <main>
-      <p>Some test text</p>
-    </main>
-    <Footer />
+      <Switch>
+        <Route exact path ="/"><Home/></Route>
+        <Route path ="/about"><About/></Route>
+        <Route path ="/services"><Services/></Route>
+        <Route path ="/contact"><Contact/></Route>
+      </Switch>
+     <Footer />
     </>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -6,15 +6,16 @@ function Header() {
     return (
         <header>
             <div className="upper-header-bar">
-                <i class="ri-phone-line"></i> <span>0207 811 8055</span>
-                <i class="ri-mail-line"></i> <span>janicewonky@emailprovider.com</span>
+                <i className="ri-mail-line ri-1.5x"></i> <span>janicewonky@emailprovider.com</span>
+                <i className="ri-phone-line ri-1.5x"></i> <span>0207 811 8055</span>
             </div>
             <div className="lower-header-bar">
-            <h1>Janice Wonky</h1>
+                <Link to="/"><h1>Janice Wonky</h1></Link>
                 <Link to="/"><p>Home</p></Link>
-                <Link to="/about"><p>About me</p></Link>
+                <Link to="/about"><p>About</p></Link>
                 <Link to="/services"><p>Services</p></Link>
-                <Link to="/contact"><p>Contact me</p></Link>
+                <Link to="/contact"><p>Contact</p></Link>
+                <i className="ri-search-line"></i>
             <input type="text" placeholder="Search website...." ></input>
             </div>
         </header>
