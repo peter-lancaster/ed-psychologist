eaac55ebc922c934cb9300cc32474b4efdc4c7ec
peter-lancaster <lancasterp2@hotmail.co.uk>
2020-06-18 15:57:53 +0100

    added fields to contact us page

--- src/components/ContactHomeGrown.js
+++ src/components/ContactHomeGrown.js
@@ -6,44 +6,58 @@ function ContactHomeGrown() {
         <div className="homegrown-contact-main">
             <p className="general-heading-font">Contact</p>
             <p>Please contact me using one of the methods below, and I will endeavour to get back to you as quickly as possible.</p>
-            <p>Telephone: 01653 648806</p>
+            <p>Telephone: 0207 811 8055</p>
             <p>Email: jennywilson@emailprovider.com</p>
-            <p>Alternatively, please complete the contact form below</p>
+            <p>Alternatively, please send me a message via the contact form below : </p>
 
             <form>
-                <label>First Name: 
+                <div className="form-element" >
+                <label for="firstName">First Name</label> 
                     <input 
                     type="text" 
                     name="firstName" 
+                    id="firstName"
                     placeholder="First Name"
                     required
                     ></input>
-                </label>
-                <label>Last Name: 
+                </div>
+                    <div className="form-element" >
+                <label for="lastName">Last Name </label>
                     <input 
                     type="text" 
                     name="lastName" 
+                    id="lastName"
                     placeholder="Last Name" 
                     required
                     ></input>
-                </label>
-                <label>Your email address: 
+                    </div>
+
+                    <div className="form-element" >
+                
+                <label for="email">Your email address </label>
                     <input 
                     type="text" 
                     name="email" 
+                    id="email"
                     placeholder="Email address" 
                     required
                     ></input>
-                </label>
-                <label>Your message: 
+                    </div>
+
+                    <div className="form-element" >
+                
+                <label for="message">Your message  </label>
                     <textarea 
                     name="message" 
+                    id="message"
                     placeholder="Please type your message here"
                     required
                     ></textarea>
-                </label>
+                    </div>
+                <div className="form-element" >
                 <button
                 >Send message</button>
+                </div>
             </form>
 
         </div>
