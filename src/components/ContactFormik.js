e9c519ddd7771da28de0097e93845784facf9411
peter-lancaster <lancasterp2@hotmail.co.uk>
2020-06-20 14:30:53 +0100

    further formik form improvements

--- src/components/ContactFormik.js
+++ src/components/ContactFormik.js
@@ -6,13 +6,33 @@ const ContactFormik = () => {
     // Pass the useFormik() hook initial form values and a submit function that will
     // be called when the form is submitted
     
-    const formik = useFormik({
+    const validate = (values) => {
 
+      const errors = {};
+
+      if (!values.firstName) {
+        errors.firstName = "First name is required"
+      }
+
+      if (!values.lastName) {
+        errors.lastName = "Last name is required"        
+      }
+
+      if (!values.email) {
+        errors.email = "Email address is required"
+      }
+
+      return errors
+    }
+
+    const formik = useFormik({
       initialValues: {
         firstName: "",
         lastName: "",
         email: '',
       },
+
+      validate,
 
       onSubmit: values => {
         alert(JSON.stringify(values, null, 2));
@@ -27,8 +47,7 @@ const ContactFormik = () => {
       <p>Telephone: 0207 811 8055</p>
       <p>Email: jennywilson@emailprovider.com</p>
       <p>Alternatively, please send me a message via the contact form below : </p>
-      
-      
+            
       <form onSubmit={formik.handleSubmit}>
       <div className="form-element" >
         <label htmlFor="firstName">First Name</label>
@@ -37,8 +56,10 @@ const ContactFormik = () => {
           name="firstName"
           type="text"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.firstName}
           />
+          {(formik.touched.firstName && formik.errors.firstName) ? <div>{formik.errors.firstName}</div> : null}
           </div>
         <div className="form-element" >
         <label htmlFor="lastName">Last Name</label>
@@ -47,9 +68,11 @@ const ContactFormik = () => {
           name="lastName"
           type="text"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.lastName}
           />
-                    </div>
+           {(formik.touched.lastName && formik.errors.lastName) ? <div>{formik.errors.lastName}</div> : null}
+          </div>
         <div className="form-element" >
         <label htmlFor="email">Email Address</label>
         <input
@@ -57,9 +80,11 @@ const ContactFormik = () => {
           name="email"
           type="email"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.email}
         />
-                  </div>
+          {(formik.touched.email && formik.errors.email) ? <div>{formik.errors.email}</div> : null}
+        </div>
         <div className="form-element" >
         <button type="submit">Submit</button>
         </div>
