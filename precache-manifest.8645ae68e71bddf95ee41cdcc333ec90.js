96bc80fcd725f2970f3bb38e5fcdfc7232f11fdb
peter-lancaster <lancasterp2@hotmail.co.uk>
2020-06-27 10:21:41 +0100

    Updates

---
+++ precache-manifest.8645ae68e71bddf95ee41cdcc333ec90.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d60ff373eafdab067f63edf6d064d3",
+    "url": "/ed-psychologist/index.html"
+  },
+  {
+    "revision": "8065563739d31ffe13bb",
+    "url": "/ed-psychologist/static/css/2.00b5ec64.chunk.css"
+  },
+  {
+    "revision": "a2aff0f650520c081005",
+    "url": "/ed-psychologist/static/css/main.f9718c59.chunk.css"
+  },
+  {
+    "revision": "8065563739d31ffe13bb",
+    "url": "/ed-psychologist/static/js/2.a577d916.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/ed-psychologist/static/js/2.a577d916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2aff0f650520c081005",
+    "url": "/ed-psychologist/static/js/main.77ded4ee.chunk.js"
+  },
+  {
+    "revision": "83c7b271775925e06f01",
+    "url": "/ed-psychologist/static/js/runtime-main.c8ac3576.js"
+  },
+  {
+    "revision": "31d28485e1cf7369272270fd730327c0",
+    "url": "/ed-psychologist/static/media/remixicon.31d28485.eot"
+  },
+  {
+    "revision": "881fbc46361e0c0e5f003c159b2f3005",
+    "url": "/ed-psychologist/static/media/remixicon.881fbc46.woff"
+  },
+  {
+    "revision": "888e61f04316f10bddfff7bee10c6dd0",
+    "url": "/ed-psychologist/static/media/remixicon.888e61f0.ttf"
+  },
+  {
+    "revision": "95138f36e015ad912c37db92164f5844",
+    "url": "/ed-psychologist/static/media/remixicon.95138f36.svg"
+  },
+  {
+    "revision": "9915fef980fa539085da55b84dfde760",
+    "url": "/ed-psychologist/static/media/remixicon.9915fef9.woff2"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ed-psychologist/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ed-psychologist/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ed-psychologist/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ed-psychologist/static/media/slick.f97e3bbf.svg"
+  }
+]);
